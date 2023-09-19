8013a767a78b5be8a66d4c1ae9c9571d44c6baee
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-09-19 11:59:48 +0200

    gg

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
-import mysql from "mysql2"
+import mysql from "mysql2";
+import "dotenv/config";
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    database: "users_db",
-    password: "qwer!234",
-    multipleStatements: true
-})
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
+  multipleStatements: true
+});
 
-export default connection
+export default connection;
