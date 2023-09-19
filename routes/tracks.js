4727f695c33c80d1759cc6406646fb31bcf6b62d
Jaes98 <marcus.jaes@gmail.com>
2023-09-19 23:21:31 +0200

    flere joins til label/genre tilt testing

--- routes/tracks.js
+++ routes/tracks.js
@@ -11,6 +11,15 @@ tracksRouter.get("/", (request, response) => {
     -- FROM tracks
     -- JOIN genres ON tracks.genreId = genres.id
     -- JOIN labels ON tracks.labelId = labels.id;
+
+-- skal ha testet disse
+
+    --SELECT a.artist_id, a.artist_name, g.genre_name, l.labelName
+    --FROM artists a
+    --JOIN artist_genre ag ON a.artist_id = ag.artist_id
+    --JOIN genres g ON ag.genre_id = g.id
+    --LEFT JOIN artist_label al ON a.artist_id = al.artist_id
+    --LEFT JOIN labels l ON al.label_id = l.id;
     SELECT * FROM tracks;
     `;
   connection.query(query, (error, results, fields) => {
