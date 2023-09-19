62091310127d831b67d3d30ae01a11d31b991789
Marcus Jaes <marcus.jaes@gmail.com>
2023-09-19 12:32:15 +0200

    Update artists.js

--- routes/artists.js
+++ routes/artists.js
@@ -5,7 +5,11 @@ const artistsRouter = Router();
 
 // READ all artists
 artistsRouter.get("/", (request, response) => {
-  const query = "SELECT * FROM artists";
+  const query = 
+    /*sql*/
+    `SELECT artists.artist_id, artists.artist_name, genres.genre_name FROM artists 
+    JOIN artist_genre ON artists.artist_id = artist_genre.artist_id
+    JOIN genres ON artist_genre.genre_id = genres.genre_id;`;
   connection.query(query, (error, results, fields) => {
     if (error) {
       console.log(error);
