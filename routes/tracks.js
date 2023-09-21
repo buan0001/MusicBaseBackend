2c7cfcf95db82b21440c43a50266b9c88d0ae5ef
Marcus Jaes <marcus.jaes@gmail.com>
2023-09-21 11:01:55 +0200

    track search route

--- routes/tracks.js
+++ routes/tracks.js
@@ -3,6 +3,7 @@ import connection from "../database.js";
 
 const tracksRouter = Router();
 
+// Get all tracks
 tracksRouter.get("/", (request, response) => {
   const query =
     /*sql*/
@@ -24,14 +25,42 @@ tracksRouter.get("/", (request, response) => {
   });
 });
 
-// GET A SINGLE TRACK BY ID
+// Search after track title
+tracksRouter.get("/search", (request, response) => {
+  const query = request.query.q;
+  const queryString = /*sql*/ `
+    SELECT DISTINCT tracks.*,
+    artists.name as artistName,
+    artists.id as artistID,
+    albums.title as albumTitle,
+    albums.id as albumID
+FROM tracks
+INNER JOIN artists_tracks ON tracks.id = artists_tracks.track_id
+INNER JOIN artists ON artists_tracks.artist_id = artists.id
+INNER JOIN albums_tracks ON tracks.id = albums_tracks.track_id
+INNER JOIN albums ON albums_tracks.albums_id = albums.id
+WHERE tracks.title LIKE ?;
+    `;
+  const values = [`%${query}%`];
+  connection.query(queryString, values, (error, results) => {
+    if (error) {
+      console.error(error);
+      response.status(500).json({ message: "Error occured" });
+    } else {
+      response.json(results);
+    }
+  });
+});
 
+// Get a single track
 tracksRouter.get("/:id", (req, res) => {
   const id = req.params.id;
   const queryString = /* sql */ `
         SELECT tracks.*,
             artists.name AS artistName,
-            artists.id AS artistId
+            artists.id AS artistId,
+            albums.title as albumTitle,
+            albums.id as albumID
             FROM tracks
             INNER JOIN artists_tracks ON tracks.id = artists_tracks.trackID
             INNER JOIN artists ON artists_tracks.artistID = artists.id
@@ -47,18 +76,3 @@ tracksRouter.get("/:id", (req, res) => {
 });
 
 export default tracksRouter;
-
-// tracksRouter.get("/search", (request, response) => {
-// 	const query = request.query.q;
-// 	const queryString = /*sql*/ `
-//     SELECT * FROM tracks WHERE name LIKE ? ORDER BY name;`;
-// 	const values = [`%${query}%`];
-// 	connection.query(queryString, values, (error, results) => {
-// 		if (error) {
-// 			console.error(error);
-// 			response.status(500).json({ message: "Error occured" });
-// 		} else {
-// 			response.json(results);
-// 		}
-// 	});
-// });
