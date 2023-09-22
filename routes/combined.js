0ec12b289e4f0eb1a6d5c01f7a104c5b5dbe5e7c
buan0001 <123996422+buan0001@users.noreply.github.com>
2023-09-22 12:24:31 +0200

    get all

--- routes/combined.js
+++ routes/combined.js
@@ -4,28 +4,40 @@ import { tryExcecute } from "../helpers.js";
 
 const combinedRouter = Router();
 
-app.get("/search", async (req, res) => {
-    const query = req.query.q;
-    console.log(query);
-  
-    // SELECT  artists.id, artists.name, artists.birthdate as time FROM artists
-    const artistString = /*sql*/ `
+combinedRouter.get("/", async (req, res) => {
+  const artistString = /*sql*/ `SELECT  * FROM artists;`;
+
+  const trackString = /*sql*/ `SELECT  * FROM tracks;`;
+
+  const albumString = /*sql*/ ` SELECT  * FROM albums;`;
+
+  const artists = await tryExcecute(artistString);
+  const tracks = await tryExcecute(trackString);
+  const albums = await tryExcecute(albumString);
+  res.json({"artists":artists, "tracks":tracks, "albums":albums});
+});
+
+
+combinedRouter.get("/search", async (req, res) => {
+  const query = req.query.q;
+  console.log(query);
+
+  // SELECT  artists.id, artists.name, artists.birthdate as time FROM artists
+  const artistString = /*sql*/ `
          SELECT  * FROM artists
           where artists.name like ?`;
-  
-    const trackString = /*sql*/ `SELECT  * FROM tracks
+
+  const trackString = /*sql*/ `SELECT  * FROM tracks
           where tracks.title like ?`;
-  
-    const albumString = /*sql*/ ` SELECT  * FROM albums
+
+  const albumString = /*sql*/ ` SELECT  * FROM albums
           where albums.title like ?;`;
-  
-    const values = [`%${query}%`];
-    const artists = await tryExcecute(artistString, values);
-    const tracks = await tryExcecute(trackString, values);
-    const albums = await tryExcecute(albumString, values);
-    // const all = [... artists, ...tracks, ...albums]
-    // console.log("ALL:",all);
-    res.json([artists, tracks, albums]);
-  });
-  
-  export default combinedRouter
+
+  const values = [`%${query}%`];
+  const artists = await tryExcecute(artistString, values);
+  const tracks = await tryExcecute(trackString, values);
+  const albums = await tryExcecute(albumString, values);
+  res.json({"artists":artists, "tracks":tracks, "albums":albums});
+});
+
+export default combinedRouter;
